5e178899cfe41a89dd406bc27f85dbfd3a422478
VishalThapaliya <vishal.thapaliya@gmail.com>
2025-03-11 22:33:00 +0100

    Fetch movies and create movie card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,59 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Search from './components/Search'
+import Spinner from './components/spinner';
+import MovieCard from './components/MovieCard';
+
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    // Authorization: `Bearer API_KEY`
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZmY5Yzg3ZDJiZTM2NTA2YjZmNTI1MDA1ZjdjYjliOSIsIm5iZiI6MTY3NzQyOTk3Mi4wNiwic3ViIjoiNjNmYjhjZDQ5NmUzMGIwMDgzMjY5MjY5Iiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.U90qGoOu2pZvQeY8EyuhNW11YRu_CQd3ao1JpiLzRoU'
+  }
+};
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  
+  const fetchMovies = async () => {
+    setIsLoading(true);
+    setErrorMessage('');
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      if(!response.ok) {
+        throw new Error('Failed to fetch movies');
+      }
+
+      const data = await response.json();
+      
+      if(data.Respons === 'False') {
+        setErrorMessage(data.Error || 'Failed to fetch movies');
+        setMovies([]);
+        return;
+      }
+
+      setMovies(data.results || []);
+
+    } catch(error) {
+      console.error(`Error while fetching movies ${error}`);
+      setErrorMessage('Error fetching movies. Please try again later.')
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
   return (
     <main>
       <div className="pattern"/>
@@ -12,9 +62,28 @@ const App = () => {
         <header>
           <img src="./hero.png" alt="Hero Banner" />
           <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
 
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <section className="all-movies">
+          <h2 className='mt-40'>All Movies</h2>
+
+          { isLoading ? (
+            <Spinner />
+          ) : errorMessage ? (
+            <p className='text-red-500'>{errorMessage}</p>
+          ) : (
+            <ul>
+              {movies.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+                
+              ))}
+            </ul>
+          )}
+          
+        </section>
+        
       </div>
     </main>
   )
